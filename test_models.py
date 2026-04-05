0c7de3f0976810405b54b1d2154854a5904752d7
AhmedTasnia <a.tasnia03@gmail.com>
2026-04-05 21:40:44 +0600

    fixed bug

---
+++ test_models.py
@@ -0,0 +1,5 @@
+import google.generativeai as genai
+import os
+
+api_key = os.environ.get("GEMINI_API_KEY", "") # I don't have their key, but I might be able to list models without a key? No, needs key.
+# I'll just print the help or doc for genai models
